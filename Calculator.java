f1ea7870c9719b09fbedd82837900ab1520ce2f9
kostasevan <sdi1400042@di.uoa.gr>
2019-03-17 18:08:42 +0200

    Working grammar

--- Calculator.java
+++ Calculator.java
@@ -2,14 +2,16 @@ import java.io.InputStream;
 import java.io.IOException;
 
 class Calculator {
-
+//1&(2&(2^8)&(2^3))
 	private int lookaheadToken;
 	private int tempSum;
+	private int openPar;
 	private InputStream in;
 
 	public Calculator(InputStream in) throws IOException {
 		this.in = in;
 		this.tempSum = 0;
+		this.openPar = 0;
 		lookaheadToken = in.read();
 	}
 
@@ -31,60 +33,80 @@ class Calculator {
 	}
 
 	private void Expr() throws IOException, ParseError {
+	    System.out.println("EXPR");
 		int a;
-		if(lookaheadToken < '0' || lookaheadToken > '9')
-		    throw new ParseError();
+		//if(lookaheadToken < '0' || lookaheadToken > '9')
+		//    throw new ParseError();
 		a = lookaheadToken - '0';
-		consume(lookaheadToken);
-		if (lookaheadToken == ')')
-			return;
-		else{
-			Term();
-			Expr2();
-			return;
-		}
+		//consume(lookaheadToken);
+		Term();
+		Expr2();
+		return;
 	}
 
 	private void Expr2() throws IOException, ParseError {
-		if(lookaheadToken == ')' || lookaheadToken == '\n' || lookaheadToken == -1)
+        System.out.println("EXPR2");
+        if(lookaheadToken == ')' ){
+            openPar--;
+            consume(lookaheadToken);
+            return;
+        }
+        if( lookaheadToken == '\n' || lookaheadToken == -1)
 			return;
+
 		if(lookaheadToken != '&')
-			throw new ParseError();
-		consume('&');
+			return;
+		consume(lookaheadToken);
 		Term();
 		Expr2();
 	}
 
 	private void Term() throws IOException, ParseError {
+        System.out.println("TERM");
 		int a;
-		if(lookaheadToken < '0' || lookaheadToken > '9')
-		    throw new ParseError();
-		a = lookaheadToken - '0';
-		consume(lookaheadToken);
-		Term2();
+		if (lookaheadToken >= '0' && lookaheadToken <= '9')
+            consume(lookaheadToken);
+        else if (lookaheadToken=='(') {
+            openPar++;
+            consume(lookaheadToken);
+            Expr();
+        }
+		if ((lookaheadToken < '0' || lookaheadToken > '9') && lookaheadToken!='(')
+		    Term2();
+		return;
 	}
 
 	private void Term2() throws IOException, ParseError {
+        System.out.println("TERM2");
 		int a;
-		if(lookaheadToken == ')' || lookaheadToken == '\n' || lookaheadToken == -1)
-			return;
+        if(lookaheadToken == ')' ){
+            openPar--;
+            consume(lookaheadToken);
+            return;
+        }
+        if( lookaheadToken == '\n' || lookaheadToken == -1)
+            return;
 		if(lookaheadToken != '^')
-			throw new ParseError();
-		consume('^');
+			return;
+		consume(lookaheadToken);
 		if (lookaheadToken == '('){
+            consume(lookaheadToken);
+            openPar++;
 			Expr();
 			return;
 		}
-		else if (lookaheadToken < '0' || lookaheadToken > '9'){
+		else if (lookaheadToken < '0' || lookaheadToken > '9')
 			throw new ParseError();
-		}
-		a = lookaheadToken - '0'; 
+		else
+    		consume(lookaheadToken);
 		Term2();
+		return;
 	}
 
 	public void parse() throws IOException, ParseError {
+        System.out.println("PARSE");
 		Expr();
-		if (lookaheadToken != '\n' && lookaheadToken != -1)
+		if ((lookaheadToken != '\n' && lookaheadToken != -1) || openPar!=0)
 			throw new ParseError();
 	}
 
